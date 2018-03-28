182160ffd5922b5c0500e5f55154255ae62d616e
Brent Labasan <BrentLabasan@gmail.com>
2018-03-28 15:32:20 -0700

    end of https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app-xplat/adding-model

---
+++ Migrations/20180328221959_InitialCreate.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TST_Fountain.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TotalSentDay = table.Column<decimal>(nullable: false),
+                    TotalSentHour = table.Column<decimal>(nullable: false),
+                    TotalSentMinute = table.Column<decimal>(nullable: false),
+                    TotalSentMonth = table.Column<decimal>(nullable: false),
+                    TotalSentSecond = table.Column<decimal>(nullable: false),
+                    TotalSentWeek = table.Column<decimal>(nullable: false),
+                    TotalSentYear = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Send",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Amount = table.Column<decimal>(nullable: false),
+                    TokenName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Send", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Send");
+        }
+    }
+}
