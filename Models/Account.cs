0612081475ca2a99498c27a6935130742d575145
Brent Labasan <BrentLabasan@gmail.com>
2018-04-02 10:41:13 -0700

    Account model refined

--- Models/Account.cs
+++ Models/Account.cs
@@ -9,18 +9,22 @@ namespace LabasanCryptoFountain.Models
 
         [Required]
         public string Address { get; set; }
+
+        public decimal TotalSentXlm { get; set; }
+
         public decimal TotalSentSecond { get; set; }
         public decimal TotalSentMinute { get; set; }
-
         public decimal TotalSentHour { get; set; }
-
         public decimal TotalSentDay { get; set; }
-
         public decimal TotalSentWeek { get; set; }
-
         public decimal TotalSentMonth { get; set; }
-
         public decimal TotalSentYear { get; set; }
+
+        public decimal TotalSentMaslow1 { get; set; }
+        public decimal TotalSentMaslow2 { get; set; }
+        public decimal TotalSentMaslow3 { get; set; }
+        public decimal TotalSentMaslow4 { get; set; }
+        public decimal TotalSentMaslow5 { get; set; }
 
     }
 }
